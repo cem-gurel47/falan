2725b55aafbc97fe532a47656193b2cddb6a444d
Cem Gurel <gurel.cem374@hotmail.com>
2023-07-15 01:20:01 -0400

    added aria label to arrow buttons

--- src/components/carousel/CarouselArrowButton.tsx
+++ src/components/carousel/CarouselArrowButton.tsx
@@ -15,6 +15,7 @@ const CarouselArrowButton = ({ type, onClickHandler, isVisible }: Props) => {
       } duration-300 transition-all ease-in-out absolute top-1/2 ${
         type === "prev" ? "left-4" : "right-4"
       } rounded-full bg-white p-2 shadow-md z-10 text-gray-400`}
+      aria-label={type === "prev" ? "Önceki" : "Sonraki"}
     >
       {type === "prev" ? <ChevronLeft size={24} /> : <ChevronRight size={24} />}
     </button>
